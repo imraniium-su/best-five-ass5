6d6f1465976966a995dd3882bf33688d34b6fdf8
MD IMRAN <imran2021ia@gmail.com>
2022-08-22 15:46:43 +0800

    list calculating

--- club.js
+++ club.js
@@ -34,3 +34,49 @@ document.getElementById('salah-btn').addEventListener('click', function () {
     li.innerText = 'Mohamed Salah';
     messilist.appendChild(li);
 })
+
+function getinputfieldvalue(inputid) {
+    const inputfield = document.getElementById(inputid);
+    const inputfildvaluestring = inputfield.value;
+    const inputfildvalue = parseFloat(inputfildvaluestring)
+    // inputfield.value = '';
+    return inputfildvalue;
+}
+
+function setTextElementValueById(inputid, value) {
+    const subTotalElement = document.getElementById(inputid);
+    subTotalElement.innerText = value;
+}
+function calculate() {
+    const ollist = document.getElementById('player-list').getElementsByTagName('li').length;
+    const perplayercost = getinputfieldvalue('input-player-cost');
+    const playertotalcost = ollist * perplayercost;
+    console.log(playertotalcost);
+    setTextElementValueById('total-expence', playertotalcost);
+}
+
+function calculatrtotal() {
+    const playerexpence = document.getElementById('total-expence');
+    const expencetring = playerexpence.innerText;
+    const Expence = parseFloat(expencetring);
+
+
+    const managercost = getinputfieldvalue('input-manager');
+
+    const coachcost = getinputfieldvalue('input-coach');
+    const totalpayment = Expence + managercost + coachcost;
+    console.log(totalpayment);
+
+    setTextElementValueById('total-expence-cost', totalpayment);
+}
+
+
+document.getElementById('btn-calculate').addEventListener('click', function () {
+    calculate()
+
+})
+
+document.getElementById('btn-calculate-total').addEventListener('click', function () {
+    calculatrtotal()
+
+})
