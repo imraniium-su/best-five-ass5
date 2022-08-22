30d7a6cd4679c73cf2270e858643ee9101799544
MD IMRAN <imran2021ia@gmail.com>
2022-08-22 14:28:33 +0800

    add li list js

---
+++ club.js
@@ -0,0 +1,36 @@
+document.getElementById('messi-btn').addEventListener('click', function () {
+    const messilist = document.getElementById('player-list');
+    const li = document.createElement('li');
+    li.innerText = 'Leo Messi';
+    messilist.appendChild(li);
+})
+document.getElementById('ronaldo-btn').addEventListener('click', function () {
+    const messilist = document.getElementById('player-list');
+    const li = document.createElement('li');
+    li.innerText = 'Cristiano Ronaldo';
+    messilist.appendChild(li);
+})
+document.getElementById('neymer-btn').addEventListener('click', function () {
+    const messilist = document.getElementById('player-list');
+    const li = document.createElement('li');
+    li.innerText = 'Neymar Jr';
+    messilist.appendChild(li);
+})
+document.getElementById('sakib-btn').addEventListener('click', function () {
+    const messilist = document.getElementById('player-list');
+    const li = document.createElement('li');
+    li.innerText = 'Shakib Al Hasan';
+    messilist.appendChild(li);
+})
+document.getElementById('mbappe-btn').addEventListener('click', function () {
+    const messilist = document.getElementById('player-list');
+    const li = document.createElement('li');
+    li.innerText = 'Kylian Mbappé';
+    messilist.appendChild(li);
+})
+document.getElementById('salah-btn').addEventListener('click', function () {
+    const messilist = document.getElementById('player-list');
+    const li = document.createElement('li');
+    li.innerText = 'Mohamed Salah';
+    messilist.appendChild(li);
+})
